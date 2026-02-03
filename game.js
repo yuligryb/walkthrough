abcfaaed44e9b72c559cbb539c7685646d36cee7
yuligryb <118086373+yuligryb@users.noreply.github.com>
2026-02-03 02:20:51 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,2 +1,40 @@
-// Your game code will go here!
-// Follow the README.md instructions and paste code from each step.
+// STEP 1: Basic game setup
+
+let player;
+let score = 0;
+
+const config = {
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
+    backgroundColor: '#000000',
+    physics: {
+        default: 'arcade',
+        arcade: { gravity: { y: 0 } }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+function preload() {
+    this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+    this.load.image('ship', 'https://labs.phaser.io/assets/sprites/thrust_ship2.png');
+    this.load.image('star', 'https://labs.phaser.io/assets/sprites/star.png');
+}
+
+function create() {
+    // We'll add stuff here in Step 2!
+}
+
+function update() {
+    // We'll add stuff here in Step 3!
+}
+
+const game = new Phaser.Game(config);
